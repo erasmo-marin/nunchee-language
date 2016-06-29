1b91fc2c9e928b349ef53475a41de92a58fa3fba
Vicente Canales <me@devjunk.org>
2016-06-29 19:10:35 -0400

    Add eslint configuration file

--- src/main.js
+++ src/main.js
@@ -1,37 +1,8 @@
 import _ from 'lodash';
+// return _.has(this.target,language) ? this.target[language] : this.target.original;
 
-let ntl = {
-    target: {},
-    translatable: false,
-    translate (language) {
-        if (this.translatable) {
-            return _.has(this.target,language) ? this.target[language] : this.target.original;
-        } else {
-            return this.string;
-        }
-    },
-    setDefault (language) {
-        this.default = language;
-    }
-}
-let translation = function translation(toTranslate,language) {
-    let ntlOptions = {};
-    if (toTranslate instanceof Object) {
-        ntlOptions = {
-            target: toTranslate,
-            translatable: true
-        }
-    } else {
-        ntlOptions = {
-            string: toTranslate,
-            translatable: false
-        }
-    }
-    if (language) {
-        let translator = Object.assign(Object.create(ntl),ntlOptions);
-        return translator.translate(language);
-    }
-    return Object.assign(Object.create(ntl), ntlOptions);
-};
+const translation = (function(target,language) {
+    let defaultLanguage = 'original';
+})();
 
 module.exports = translation;
